d0073b3a9e44bfdd20906b11a0bf4efb0f6a6c71
Konstantin-174 <timokhin_74@hotmail.com>
2021-05-03 08:30:02 +0500

    3st (01 lesson - added beforeEach() )

--- src/01_tests/01.test.ts
+++ src/01_tests/01.test.ts
@@ -1,39 +1,43 @@
 import {mult, splitIntoWords, sum} from './01';
 
-test("sum should be correct", () => {
-    // data
-    const a = 1;
-    const b = 2;
-    const c = 3;
+let a: number;
+let b: number;
+let c: number;
 
+beforeEach(() => {
+    a = 1;
+    b = 2;
+    c = 3;
+})
+
+test('sum should be correct', () => {
+    // data
+    a = 48
     //action
-    const result1 = sum(a,b);
-    const result2 = sum(b,c);
+    const result1 = sum(a, b);
+    const result2 = sum(b, c);
 
     //expect result
-    expect(result1).toBe(3);
+    expect(result1).toBe(50);
     expect(result2).toBe(5);
 })
 
-test("multiply should be correct", () => {
+test('multiply should be correct', () => {
     // data
-    const a = 1;
-    const b = 2;
-    const c = 3;
 
     //action
-    const result1 = mult(a,b);
-    const result2 = mult(b,c);
+    const result1 = mult(a, b);
+    const result2 = mult(b, c);
 
     //expect result
     expect(result1).toBe(2);
     expect(result2).toBe(6);
 })
 
-test("sentence should be splitted", () => {
+test('sentence should be splitted', () => {
     // data
-    const sent1 = "Hello my friend";
-    const sent2 = "JS  is my favourite language!";
+    const sent1 = 'Hello my friend';
+    const sent2 = 'JS  is my favourite language!';
 
     //action
     const result1 = splitIntoWords(sent1);
@@ -41,14 +45,14 @@ test("sentence should be splitted", () => {
 
     //expect result
     expect(result1.length).toBe(3);
-    expect(result1[0]).toBe("hello");
-    expect(result1[1]).toBe("my");
-    expect(result1[2]).toBe("friend");
+    expect(result1[0]).toBe('hello');
+    expect(result1[1]).toBe('my');
+    expect(result1[2]).toBe('friend');
 
     expect(result2.length).toBe(5);
-    expect(result2[0]).toBe("js");
-    expect(result2[1]).toBe("is");
-    expect(result2[2]).toBe("my");
-    expect(result2[3]).toBe("favourite");
-    expect(result2[4]).toBe("language");
+    expect(result2[0]).toBe('js');
+    expect(result2[1]).toBe('is');
+    expect(result2[2]).toBe('my');
+    expect(result2[3]).toBe('favourite');
+    expect(result2[4]).toBe('language');
 })
